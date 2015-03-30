41880726da7115abdbcd6717b0d72b76208a9d31
Artur Korop <rubkaarchi@gmail.com>
2015-03-30 17:30:00 +0300

    Init

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardController : MonoBehaviour
+{
+    public GameObject Background;
+
+    public int Columns = 8;
+    public int Rows = 8;
+
+    private Transform boardHolder;
+
+    public void Awake()
+    {
+        this.BoardSetup();
+    }
+
+    private void BoardSetup()
+    {
+        this.boardHolder = new GameObject("Board").transform;
+
+        for (int i = 0; i < this.Columns; i++)
+        {
+            for (int j = 0; j < this.Rows; j++)
+            {
+                var instance = Instantiate(this.Background, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(this.boardHolder);
+            }
+        }
+    }
+}
