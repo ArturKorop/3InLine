00efce045145f712f29f2837a4de9f8b3555f20d
Artur Korop <rubkaarchi@gmail.com>
2015-03-31 16:51:03 +0300

    Update

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -1,18 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 public class BoardController : MonoBehaviour
 {
-    public GameObject Background;
+    public List<GameObject> Gems;
+    public static BoardController Instance = null;
 
     public int Columns = 8;
     public int Rows = 8;
 
     private Transform boardHolder;
+    private GameObjectValue[,] gemsField;
+    private GameObjectValue currentPressed;
 
     public void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        this.gemsField = new GameObjectValue[this.Columns, this.Rows];
         this.BoardSetup();
+    }
+
+    public void GemPressed(GameObject gem)
+    {
+        GameObjectValue target = null;
+        for (int i = 0; i < this.Columns; i++)
+        {
+            for (int j = 0; j < this.Rows; j++)
+            {
+                var obj = this.gemsField[i,j];
+                if(gem == obj.Object)
+                {
+                    target = obj;
+                    break;
+                }
+            }
+        }
+        
+        if(this.currentPressed == null || this.currentPressed.Value != target.Value || this.GetDistance(this.currentPressed.Position, target.Position) > 1)
+        {
+            this.currentPressed = target;
+        }
+        else if(this.currentPressed.Object == target.Object)
+        {
+
+        }
+        else
+        {
+            Destroy(target.Object);
+            Destroy(this.currentPressed.Object);
+        }
     }
 
     private void BoardSetup()
@@ -23,9 +72,16 @@ public class BoardController : MonoBehaviour
         {
             for (int j = 0; j < this.Rows; j++)
             {
-                var instance = Instantiate(this.Background, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+                var index = Random.Range(0, this.Gems.Count);
+                var instance = Instantiate(this.Gems[index], new Vector3(i, j, 0), Quaternion.identity) as GameObject;
                 instance.transform.SetParent(this.boardHolder);
+                this.gemsField[i, j] = new GameObjectValue { Value = index, Object = instance, Position = new Point { X = i, Y = j } };
             }
         }
+    }
+
+    private float GetDistance(Point a, Point b)
+    {
+        return Mathf.Sqrt(Mathf.Abs(a.X - b.X) + Mathf.Abs(a.Y - b.Y));
     }
 }
---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct Point
+{
+    public int X;
+    public int Y;
+}
+
+public class GameObjectValue
+{
+    public GameObject Object;
+    public int Value;
+    public Point Position;
+}
